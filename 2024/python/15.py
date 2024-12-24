0710be1c9505fafdd0a5330f478a90237474b73e
ipragmatic <ipragmatic2r@gmail.com>
2024-12-24 11:55:41 -0500

    Completed Day 15

--- 2024/python/15.py
+++ 2024/python/15.py
@@ -1,50 +1,6 @@
-from helper import to_str_matrix, timeit, dirs, print_list_matrix
 from collections import deque
 
-ex1 = """##########
-#..O..O.O#
-#......O.#
-#.OO..O.O#
-#..O@..O.#
-#O#..O...#
-#O..O..O.#
-#.OO.O.OO#
-#....O...#
-##########
-
-<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
-vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
-><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
-<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
-^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
-^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
->^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
-<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
-^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
-v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
-
-
-ex2 = """########
-#..O.O.#
-##@.O..#
-#...O..#
-#.#.O..#
-#...O..#
-#......#
-########
-
-<^^>>>vv<v>>v<<"""
-
-
-ex3 = """#######
-#...#.#
-#.....#
-#..OO@#
-#..O..#
-#.....#
-#######
-
-<vv<<^^<<^^"""
+from helper import dirs, timeit, to_str_matrix
 
 
 def parse(text_data):
@@ -119,22 +75,20 @@ def double_width(_map):
 
 
 def moveable_p2(_map, r, c, dr, dc):
-    objects = []
+    objects = [[]]
     visited = set()
-    queue = deque([(r, c)])
+    queue = deque([(r + dr, c + dc)])
 
     while queue:
         nr, nc = queue.popleft()
 
-        if (
-            (nr, nc) in visited
-            or _map[nr][nc] == "#"
-            or not (0 <= nr < len(_map) and 0 <= nc < len(_map[0]))
-        ):
-            # return False, []
+        if (nr, nc) in visited or not (0 <= r < len(_map) and 0 <= c < len(_map[0])):
             continue
+        if _map[nr][nc] == "#":
+            return False, []
 
         visited.add((nr, nc))
+
         r_idx = abs(nr - r)
         if r_idx >= len(objects):
             objects.append([])
@@ -144,30 +98,24 @@ def moveable_p2(_map, r, c, dr, dc):
             continue
 
         if _map[nr][nc] == "[":
-            pair = (nr, nc), (nr, nc + 1)
+            obj = [(nr, nc), (nr, nc + 1)]
         elif _map[nr][nc] == "]":
-            pair = (nr, nc), (nr, nc - 1)
-        elif _map[nr][nc] == "@":
-            pair = [(nr, nc)]
-        for b in pair:
-            if (b[0] + dr, b[1] + dc) not in visited:
-                queue.append((b[0] + dr, b[1] + dc))
+            obj = [(nr, nc), (nr, nc - 1)]
+        else:
+            obj = [(nr, nc)]
 
-    for row in objects:
-        for r, c in row:
-            if _map[r][c] == "#":
-                return False, []
+        for pr, pc in obj:
+            if (pr + dr, pc + dc) not in visited:
+                queue.append((pr + dr, pc + dc))
     return True, objects
 
 
-def shift_p2(
-    _map, involved, dr, dc
-):  # dr and dc here are going to be steps in the direction
-    # opposite to the one taken in the traversal
+def shift_p2(_map, involved, dr, dc):
     for row in reversed(involved):
         for r, c in reversed(row):
-            br, bc = r - dr, c - dc  # reads "behind r" and "behind c"
-            _map[r][c], _map[br][bc] = _map[br][bc], _map[r][c]
+            br, bc = r - dr, c - dc
+            if _map[br][bc] != "#":
+                _map[r][c], _map[br][bc] = _map[br][bc], _map[r][c]
 
 
 def part2(text_data):
@@ -176,18 +124,16 @@ def part2(text_data):
     _map = double_width(old)
     bot = find_bot(_map)
     for step in bot_dirs:
-        print_list_matrix(_map)
-        print(step)
         r, c = bot
         dr, dc = dirs[step]
         move, involved = moveable_p2(_map, r, c, dr, dc)
-        print(*involved)
         if move:
             shift_p2(_map, involved, dr, dc)
             bot = r + dr, c + dc
-        print()
-    # print_list_matrix(_map)
-    # print(step)
+    for i in range(len(_map)):
+        for j in range(len(_map[0])):
+            if _map[i][j] == "[":
+                val += 100 * i + j
     return val
 
 
@@ -196,10 +142,8 @@ def part2(text_data):
 if __name__ == "__main__":
     with open("../puzzle_input/15.txt") as f:
         text_data = f.read()
-    # timeit(part1, text_data)
-    # timeit(part2, text_data)
-    # timeit(part2, ex1)
-    timeit(part2, ex3)
+    timeit(part1, text_data)
+    timeit(part2, text_data)
 
 # part 1 answers:
 #   - 1509074  (CORRECT)
@@ -208,4 +152,5 @@ if __name__ == "__main__":
 #       attempt 3: ~0.009848s  (changed matrix representation to a 2D list instead of a dict)
 
 # part 2 answers:
-#   -
+#   - 1521453  (CORRECT)
+#       attempt 1: ~0.027917s
